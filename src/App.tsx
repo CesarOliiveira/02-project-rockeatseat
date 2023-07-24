21a748154599aa973f1121d1e7b2a471684ec279
CesarOliiveira <cesaroliiveira8@gmail.com>
2023-07-24 07:53:55 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+     <Button variant="primary"/>
+     <Button variant="secondary"/>
+     <Button variant="success"/>
+     <Button variant="danger"/>
+     <Button/>
+
+     <GlobalStyle/>
+    </ThemeProvider>
+  )
+}
+
